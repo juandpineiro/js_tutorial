4ae0d97952ab1e1c64d5b7862eaea0526ba879f8
Juan Piñeiro <juandpineiro@gmail.com>
2019-11-26 13:26:32 -0400

    Add prototypes and inheritance

--- palindrome.js
+++ palindrome.js
@@ -3,14 +3,34 @@ function reverse(string) {
   return Array.from(string).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+// Defines a Phrase object
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function processor(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true for a palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  }
 }
 
-// Return an array of username and domain from an email
-function emailParts(email) {
-  let processedContent = email.toLowerCase();
-  return processedContent.split("@");
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
 }
+
+TranslatedPhrase.prototype = new Phrase();
